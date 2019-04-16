3a829f15b8f5bf7381ec1fdfa6b658779e39b580
timw <timwang1104@126.com>
2019-04-16 07:20:24 -0700

    add uart interface in io control module

--- hardware/src/io_circuits/uart_transmitter.v
+++ hardware/src/io_circuits/uart_transmitter.v
@@ -13,7 +13,66 @@ module uart_transmitter #(
 
     output serial_out
 );
-    assign serial_out = 1'b1;
-    assign data_in_ready = 1'b0;
-    // Place your implementation here.
+
+    // See diagram in the lab guide
+    localparam  SYMBOL_EDGE_TIME    =   CLOCK_FREQ / BAUD_RATE;
+    localparam  CLOCK_COUNTER_WIDTH =   `log2(SYMBOL_EDGE_TIME);
+
+    wire symbol_edge;
+    wire tx_running;
+    wire start;
+
+    reg [9:0] tx_shift;
+    reg [3:0] bit_counter;
+    reg [CLOCK_COUNTER_WIDTH-1:0] clock_counter;
+
+    //--|Signal Assignments|------------------------------------------------------
+
+    // Goes high at every symbol edge
+    assign symbol_edge = clock_counter == (SYMBOL_EDGE_TIME - 1);
+    assign start=(data_in_valid)&&(bit_counter==0);
+    assign tx_running=bit_counter!=0;
+
+    // Outputs
+    assign serial_out = tx_shift[0];
+    assign data_in_ready=!tx_running;
+
+    //--|Counters|----------------------------------------------------------------
+
+    // Counts cycles until a single symbol is done
+    always @ (posedge clk) begin
+        clock_counter <= (start || reset || symbol_edge) ? 0 : clock_counter + 1;
+    end
+
+    always @ (posedge clk) begin
+        if (reset) begin
+            bit_counter <= 0;
+        end 
+        else if (start) begin
+            bit_counter <= 1;
+            tx_shift<={1'b1,data_in,1'b0};
+        end 
+        else if (symbol_edge) begin
+            if(bit_counter<=10) begin
+                bit_counter <= bit_counter+1;
+                tx_shift <= {1'b1, tx_shift[9:1]};
+            end
+            else begin
+                bit_counter<=0;
+            end
+        end
+    end
+    
+    //--|Shift Register|----------------------------------------------------------
+    always @(posedge clk) begin
+        if (reset || !tx_running) begin
+            tx_shift <= 10'b1;
+        end
+        if (start) begin
+            tx_shift <= {1'b1, data_in[7:0], 1'b0};
+        end
+        else if (symbol_edge && tx_running && bit_counter < 10)
+            tx_shift <= tx_shift >> 1;
+    end
+
 endmodule
--- hardware/src/riscv_core/io_control.v
+++ hardware/src/riscv_core/io_control.v
@@ -8,27 +8,56 @@ module io_control
 	input cpu_rst,
 	input [`IO_MAP_WIDTH-1:0] adr,
 	input instr_stop,
-	output [`XLEN-1:0] dout_io
+	input [`XLEN-1:0] din_io,
+	input uart_serial_in,
+	output [`XLEN-1:0] dout_io,
+	output uart_serial_out
 );
+	wire [`XLEN-1:0] cycle_cnt;
+	wire [`XLEN-1:0] instr_cnt;
+	wire [7:0]       uart_data_in;
+	wire [7:0]       uart_data_out;
+	wire             uart_data_in_valid;
+	wire             uart_data_in_ready;
+	wire             uart_data_out_valid;
+	wire             uart_data_out_ready;
+	wire             uart_serial_in;
+	wire             uart_serial_out;
 
 	reg [`XLEN-1:0]  dout_io_reg;
 	reg counter_rst;
 
-	wire [`XLEN-1:0] cycle_cnt;
-	wire [`XLEN-1:0] instr_cnt;
-
+	reg [7:0]       uart_data_in_reg;
+	reg             uart_data_in_valid_reg;
+	reg             uart_data_out_valid_reg;
+	reg             uart_serial_in_reg;
 
 	cycle_counter m_cycle_counter(.clk(clk), .counter_rst(counter_rst), .cycle_cnt(cycle_cnt));
 	instr_counter m_instr_counter(.clk(clk), .instr_stop(instr_stop), .counter_rst(counter_rst), .instr_cnt(instr_cnt));
-
+	uart          m_uart(.clk(clk), .reset(cpu_rst), .data_in(uart_data_in), .data_in_valid(uart_data_in_valid), .data_in_ready(uart_data_in_ready), .data_out(data_out),.data_out_valid(uart_data_out_valid), .data_out_ready(uart_data_out_ready), .serial_in(uart_serial_in), .serial_out(uart_serial_out));
+	
 	always @(posedge clk) begin
 		if (cpu_rst) begin
 			dout_io_reg<=32'd0;
 			counter_rst<=1'b1;
+
+			uart_data_in_reg<=8'd0;
+			uart_data_in_valid_reg<=1'b0;
+			uart_data_out_ready_reg<=1'b0;
+			uart_serial_in_reg<=1'b0;			
 		end
 		else begin
 			if (io_en) begin
 				case(adr)
+					5'b00000: begin
+						dout_io_reg<={30'b0,uart_data_out_valid,uart_data_in_ready};
+					end
+					5'b00001: begin
+						dout_io_reg<={24'b0,uart_data_out};
+					end
+					5'b00010: begin
+						uart_data_in_reg<=din_io[7:0];
+					end
 					5'b00100: begin
 						dout_io_reg<=cycle_cnt;
 					end
@@ -56,6 +85,7 @@ module io_control
 	end
 
 	assign dout_io=dout_io_reg;
+	assign uart_data_in=uart_data_in_reg;
 
 
 endmodule
